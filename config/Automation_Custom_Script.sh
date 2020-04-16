094efb48f8d3a13f5709c2bc694ed912e1d75b7e
Eirik Sand <eirik.sand@ardoq.com>
2020-04-16 21:46:56 +0200

    Setup

--- config/Automation_Custom_Script.sh
+++ config/Automation_Custom_Script.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-# Install packages
+# Install common packages
 apt-get --assume-yes install curl
+
+# Install Docker
+curl -sSL https://get.docker.com | sh
+usermod -aG docker dietpi
+
+# Install Docker Compose
 apt-get --assume-yes install docker-compose
 
 # Setup Docker Compose
 mkdir -p /opt/docker-compose/services
 cd /opt/docker-compose
-curl -s https://raw.githubusercontent.com/ezand/dietpi-base-image/master/docker-compose/docker-compose.service --output docker-compose.service
-curl -s https://raw.githubusercontent.com/ezand/dietpi-base-image/master/docker-compose/docker-compose.yml --output docker-compose.yml
+curl -fsSL https://raw.githubusercontent.com/ezand/dietpi-base-image/master/docker-compose/docker-compose.service --output docker-compose.service
+curl -fsSL https://raw.githubusercontent.com/ezand/dietpi-base-image/master/docker-compose/docker-compose.yml --output docker-compose.yml
 systemctl enable /opt/docker-compose/docker-compose.service
 docker-compose build
 systemctl start docker-compose
